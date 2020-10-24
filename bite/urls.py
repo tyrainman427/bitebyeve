9ac8a69bcf1b6133b089b2ae5642dd7005d529c2
tyrainman427 <tyraineytech@gmail.com>
2020-10-24 10:44:07 -0400

    added cart

--- bite/urls.py
+++ bite/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', include('product.urls')),
     path('cart/',include('cart.urls')),
     path('admin/', admin.site.urls),
+    path('s3direct/', include('s3direct.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
